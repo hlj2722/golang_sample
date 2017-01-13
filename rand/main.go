fab429e2e99758b0ac8b84842b0cd6e0f2a2a8ef
hlj2722 <hlj2722@163.com>
2017-01-13 10:24:06 +0800

    添加生成随机数代码示例

---
+++ rand/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+)
+
+func main(){
+	b_rand:= krand(15,3)
+	fmt.Println("s_rand:",string(b_rand))
+}
+
+/**
+ * size 随机码的位数
+ * kind 0    // 纯数字
+        1    // 小写字母
+        2    // 大写字母
+        3    // 数字、大小写字母
+*/
+func krand(size int, kind int) []byte {
+    ikind, kinds, result := kind, [][]int{[]int{10, 48}, []int{26, 97}, []int{26, 65}}, make([]byte, size)
+    is_all := kind > 2 || kind < 0
+    rand.Seed(time.Now().UnixNano())
+    for i := 0; i < size; i++ {
+        if is_all { // random ikind
+            ikind = rand.Intn(3)
+        }
+        scope, base := kinds[ikind][0], kinds[ikind][1]
+        result[i] = uint8(base + rand.Intn(scope))
+    }
+    return result
+}
